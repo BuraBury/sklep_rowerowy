d3c3df9baab793019cedf7578c168125f42f1dab
Paulina Bury <burabury@MacBook-Pro-Paulina.local>
2020-10-04 13:43:29 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function downloadProducts() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onload = function() {
+        var section = document.querySelector("main section");
+        var productsList = JSON.parse(this.responseText);
+
+        var productsTable = "<table>";
+
+        for(var i = 0; i < productsList.length; i++) {
+            // section.innerHTML += productsList[i].name;
+            productsTable += `<tr><td>${productsList[i].name}</td><td>${productsList[i].price}</td></tr>`;
+        }
+
+        productsTable += "</table>";
+        section.innerHTML = productsTable;
+    }
+
+    xhttp.open("GET", "rowery.json", true);
+    xhttp.send();
+}
+
+downloadProducts();
+
+
